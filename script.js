ed14aeb711b6007101c575db3163b077c7b4cc5c
xItsSky <emo.quentin@gmail.com>
2025-02-07 09:51:05 +0100

    feat: add ratio script

---
+++ script.js
@@ -0,0 +1,59 @@
+var pointUnitRatioPerUser = [];
+var unitPolulations = [1, 1, 1, 1, 2, 4, 5, 6, 5, 8];
+var rows = document.getElementsByClassName('w100')[0].rows;
+for(let i=1;i<rows.length-1;i++) {
+    var row = rows.item(i);
+    var rowElements = row.cells;
+    setUserPointUnitRatio(rowElements);
+}
+
+function setUserPointUnitRatio(rowElements) {
+    var user = getUserName(row.cells.item(0).textContent);
+    var points = Number(row.cells.item(1).textContent.replaceAll('.', ''));
+    var userUnitPopulation = 0;
+    for(let j=2;j<rowElements.length-4;j++) {
+        var nbOfUnit = rowElements.item(j).textContent;
+        if(nbOfUnit.includes('?')) {
+            userUnitPopulation = '?';
+        } else {
+            userUnitPopulation+=(Number(nbOfUnit) * unitPolulations[j-2]);
+        }
+    }
+    var ratio = userUnitPopulation !== '?' ? parseFloat(userUnitPopulation / points).toFixed(2) : '?';
+    pointUnitRatioPerUser.push({user: user, point: points, unitPopulation: userUnitPopulation, ratio: ratio});
+}
+
+clearDom('ratioTable');
+insertInDom('ratioTable', pointUnitRatioPerUser);
+
+function getUserName(user) {
+    return user.replaceAll('\n', '').trim();
+}
+
+function clearDom(id) {
+    var table = document.getElementById("ratioTable");
+    if(table) {
+        table.remove();
+    }
+}
+
+function insertInDom(id, data) {
+    let tableHTML = '<table id="' + id + '" class="vis w100"><tr>';
+
+    Object.keys(data[0]).forEach(key => {
+        tableHTML += `<th>${key}</th>`;
+    });
+
+    tableHTML += '</tr>';
+
+    data.forEach(item => {
+        tableHTML += '<tr>';
+        Object.values(item).forEach(value => {
+            tableHTML += `<td>${value}</td>`;
+        });
+        tableHTML += '</tr>';
+    });
+
+    tableHTML += '</table>';
+    document.getElementById('contentContainer').insertAdjacentHTML('afterend', tableHTML);
+}
